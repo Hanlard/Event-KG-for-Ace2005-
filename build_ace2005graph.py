bc9935a2d2bf8c2fdf4bbd9a18db9abd4f1db01f
Hanlard <491377729@qq.com>
2020-03-01 01:13:24 +0800

    2020/3/1第一次提交，完成了知识图谱的搭建

--- build_ace2005graph.py
+++ build_ace2005graph.py
@@ -15,8 +15,8 @@ class ACE2005Graph:
         self.g = Graph(
             host="127.0.0.1",  # neo4j 搭载服务器的ip地址，ifconfig可获取到
             http_port=7474,  # neo4j 服务器监听的端口号
-            user="neo4j",  # 数据库user name，如果没有更改过，应该是neo4j
-            password="Nzhanghan0512")
+            user="需要修改",  # 数据库user name，如果没有更改过，应该是neo4j
+            password="需要修改")
 
         relations_type = []# role
         nodes_type = ['event']# 'event' and entity_type
